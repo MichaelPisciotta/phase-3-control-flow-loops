56a92bd17faec92a09a9a50aa796be9f593275d4
MichaelPisciotta <mfp56@miami.edu>
2021-09-20 18:49:13 -0400

    Done.

--- looping.rb
+++ looping.rb
@@ -1,24 +1,48 @@
-def happy_new_year
-  # your code here
-end
+
+def happy_new_year(counter)
+  counter = 10
+  until counter == 0
+    puts "${counter}"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end 
+
+
+
+# # function happyNewYear() {
+# #   let counter = 10;
+# #   while (counter > 0) {
+# #     console.log(counter);
+# #     counter--;
+# #   }
+# #   console.log("Happy New Year!");
+# # }
+
+
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
-  end
-end
 
-def fizzbuzz_printer
-  # your code here
-end
+# def fizzbuzz(num)
+#   if num % 3 == 0 && num % 5 == 0
+#     "FizzBuzz"
+#   elsif num % 3 == 0
+#     "Fizz"
+#   elsif num % 5 == 0
+#     "Buzz"
+#   else
+#     num
+#   end
+# end
 
-def reverse_string(str)
-  # your code here
-end
+# def fizzbuzz_printer
+#   # your code here
+# end
+
+
+
+# def reverse_string(str)
+#   # your code here
+# end
